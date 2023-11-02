cf10d1239289512c1625b4e0646b9ec2123142c6
Patricia Sheatsley <PSheatsley@esri.com>
2023-11-02 13:52:01 -0700

    Add copyright
    Copyright 2023 Esri is added to the header of each file.

--- CustomPresentationLibrary/lua/depare01.lua
+++ CustomPresentationLibrary/lua/depare01.lua
@@ -1,3 +1,18 @@
+--[[ * Copyright 2023 Esri
+ *
+ * Licensed under the Apache License Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ]]
+
 -- DEPDW;white;0.2800;0.3100;80.000;255;255;255;0.292854  
 -- DEPMD;pale_blue;0.2600;0.2900;65.000;255;255;255;0.287164  
 -- DEPMS;light_blue;0.2300;0.2500;55.000;221;235;247;0.664280  
--- CustomPresentationLibrary/lua/quapos01.lua
+++ CustomPresentationLibrary/lua/quapos01.lua
@@ -1,3 +1,18 @@
+--[[ * Copyright 2023 Esri
+ *
+ * Licensed under the Apache License Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ]]
+
 -- Conditional Symbology Procedure QUAPOS01
 -- It applies to (1) S-57 Object Class "land area" (LNDARE), as point and line (2) S-57 Object Class coast line” (COALNE), line only
 function quapos01(feature, context, symbology)
